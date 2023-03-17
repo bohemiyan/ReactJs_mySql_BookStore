25d5b335c90792c40b1391bd2780af17e706282b
Chirag Mandal <chiragmndl@gmail.com>
2023-03-18 03:03:57 +0530

    Update Update.jsx

--- client/src/pages/Update.jsx
+++ client/src/pages/Update.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 import  {React, useState,useEffect} from "react";
-// import { useContext } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-// import { StoreContext } from "../Contexts/StoreContext";
+
 
 const Update = () => {
   const [book, setBook] = useState({});
